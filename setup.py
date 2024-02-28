83ab9faaa51fc538c0c5a58090fda87c4f2ad8a4
Hetari <hetari4py@gmail.com>
2024-02-29 00:02:08 +0300

    Rename the app

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
-    name='youtube_downloader',
-    version='1.0',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'youtube-downloader=youtube_downloader.cli:main',
-        ],
-    },
-)
+# setup(
+#     name='uTube',
+#     version='1.0',
+#     packages=find_packages(),
+#     entry_points={
+#         'console_scripts': [
+#             'youtube-downloader=uTube.cli:main',
+#         ],
+#     },
+# )
