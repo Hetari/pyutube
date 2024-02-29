bdd03a17977214a386674fb68cc17233a50e738b
Hetari <hetari4py@gmail.com>
2024-02-29 12:03:24 +0300

    Add the download_vide start function

--- uTube/cli.py
+++ uTube/cli.py
@@ -4,8 +4,11 @@ from .utils import (
     __version__,
     console,
     welcome,
+    clear,
     is_internet_available,
 )
+
+from .downloader import download_vide
 
 import typer
 import rich
@@ -17,3 +20,14 @@ app = typer.Typer()
 @app.command(name="info", help="Get information about the app")
 def info():
     console.print(f"{__app_name__} v {__version__}", style="info")
+
+
+@app.command(name="download", help="Download a YouTube video")
+def download(url: str = typer.Argument(...)):
+    clear()
+    if is_internet_available():
+        console.print("✅ There is internet connection", style="info")
+
+        download_vide(url)
+    else:
+        console.print("❗ No internet connection", style="danger")
--- uTube/downloader.py
+++ uTube/downloader.py
@@ -1,1 +1,10 @@
 # Logic for downloading YouTube videos
+import time
+import pytube
+from pytube import YouTube
+from rich.progress import track
+
+
+def download_vide(url):
+    for _ in track(range(20), description="Processing..."):
+        time.sleep(1)
