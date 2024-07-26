f8497ee0ca3324d4f884d815ce201d042bd9276b
Hetari <hetari4py@gmail.com>
2024-07-26 22:28:46 +0300

    improve the bash command

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# remove the previous build files
+rm -rf dist
+
 # Uninstall the package 'pyutube' with the '-y' flag to confirm the uninstallation without user prompt
 pip uninstall pyutube -y
 
@@ -6,3 +9,8 @@ python setup.py bdist_wheel
 
 # Install the wheel distribution package located in the 'dist' directory
 pip3 install dist/*
+
+# remove any random folder in all levels
+find . -name '__pycache__' -exec rm -rf {} +
+rm -rf pyutube.egg-info
+rm -rf build
